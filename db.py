012a250abaf6a588ed676218ba1b85ee67c08d66
Aryan Shankhapal <102903816+aryans3@users.noreply.github.com>
2023-11-02 22:09:17 +0530

    Delete db.py

--- db.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-connection_string = "mongodb+srv://adishiro:gintoki@cluster0.wlmssnp.mongodb.net/SPACE?retryWrites=true&w=majority"
-
-client = MongoClient(connection_string)
-db = client.get_database("SPACE")
-users = db.get_collection("users")
